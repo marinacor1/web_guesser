2e0909eb85ed26ea6be2f80521dff0e588007fa5
marinacor1 <marinaspam1234@gmail.com>
2016-03-14 12:35:54 -0600

    too low and too high works

--- web_guesser.rb
+++ web_guesser.rb
@@ -3,11 +3,25 @@ require 'sinatra/reloader'
 
   number = rand(101)
 
+  def guess_check(guess, number)
+    difference = (guess - number)
+    if difference >= 5
+      message = "Way too high!"
+    elsif difference < 5 && difference > 0
+      message = "Too high!"
+    elsif difference <= -5
+      message = "Way too low!"
+    elsif difference < 0 && difference > -5
+      message = "Too low!"
+    else
+      message = "You got it right!"
+    end
+    message
+  end
+
 get '/' do
   guess = params["guess"].to_i
-  if guess > number
-    message = "Too high!"
-  end
+  message = guess_check(guess, number)
     erb :index, :locals => {:number => number,
                             :message => message }
 end
