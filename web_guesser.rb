c4ccb04ff27a572fe87e68d4fbede0df2e849138
marinacor1 <marinaspam1234@gmail.com>
2016-03-14 12:00:30 -0600

    adding functionality for too high

--- web_guesser.rb
+++ web_guesser.rb
@@ -4,5 +4,10 @@ require 'sinatra/reloader'
   number = rand(101)
 
 get '/' do
-  erb :index, :locals => {:number => number}
+  guess = params["guess"].to_i
+  if guess > number
+    message = "Too high!"
+  end
+    erb :index, :locals => {:number => number,
+                            :message => message }
 end
