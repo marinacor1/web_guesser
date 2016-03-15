2762e556148ee3dbbda6e53a1f37230176f2cec9
marinacor1 <marinaspam1234@gmail.com>
2016-03-14 18:33:36 -0600

    works with hex colors

--- web_guesser.rb
+++ web_guesser.rb
@@ -4,25 +4,25 @@ require 'sinatra/reloader'
 SECRET_NUMBER = rand(101)
 set :number, SECRET_NUMBER #defaults at initialize
 set :message, ""
-set :color_output, "white"
+set :color_output, "#FFFFFF"
 
   def guess_check(guess)
     difference = (guess - settings.number)
     if difference >= 5
       message_assignment("Way too high!")
-      color_selector("red")
+      color_selector("#FF0000")
     elsif difference < 5 && difference > 0
       message_assignment "Too high!"
-      color_selector("pink")
+      color_selector("#FF00FF")
     elsif difference <= -5
       message_assignment "Way too low!"
-      color_selector("red")
+      color_selector("#FF0000")
     elsif difference < 0 && difference > -5
       message_assignment "Too low!"
-      color_selector("pink")
+      color_selector("#FF00FF")
     else
       message_assignment "You got it right!"
-      color_selector("green")
+      color_selector("#00FF00")
     end
   end
 
@@ -38,7 +38,6 @@ get '/' do
   guess = params["guess"].to_i #takes in input
   number = settings.number #is defined above and never changes
   guess_check(guess) #is run within runner and determines message and color
-  # color_output = color_selector(color_output) #
   erb :index, :locals => {:number => number,
                           :message => settings.message,
                           :color_output => settings.color_output}
