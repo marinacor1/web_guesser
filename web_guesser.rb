31bd7a05722c10f387cf48e47696265b47036ff3
marinacor1 <marinaspam1234@gmail.com>
2016-03-21 11:52:12 -0600

    adding notes from mod 2 class

--- web_guesser.rb
+++ web_guesser.rb
@@ -16,7 +16,7 @@ set :color_output, "#FFFFFF"
       color_selector("FFCCCC") #light pink
     elsif difference <= -5
       message_assignment "Way too low!"
-      color_selector("#FF0000") #red 
+      color_selector("#FF0000") #red
     elsif difference < 0 && difference > -5
       message_assignment "Too low!"
       color_selector("FFCCCC") #light pink
@@ -34,11 +34,11 @@ set :color_output, "#FFFFFF"
     settings.color_output = color_output
   end
 
-get '/' do
-  guess = params["guess"].to_i #takes in input
+get '/' do #get me something from... get '/people' get'/posts'. post requests do a redirect(sends request to post, go into program and find corresponding file and redisplays to a get). get get something to display
+  guess = params["guess"].to_i #takes in input. params is a hash. guess is a string turned into integer. 
   number = settings.number #is defined above and never changes
   guess_check(guess) #is run within runner and determines message and color
-  erb :index, :locals => {:number => number,
+  erb :index, :locals => {:number => number, #erb :index renders erb file
                           :message => settings.message,
                           :color_output => settings.color_output}
 end
